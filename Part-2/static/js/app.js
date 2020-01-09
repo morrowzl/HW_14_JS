8c2513ab647915987d7a1d62db22b20704d56a72
Zachary Morrow <morrowzl@vcu.edu>
2020-01-09 18:34:10 -0500

    checkpoint

--- Part-2/static/js/app.js
+++ Part-2/static/js/app.js
@@ -1,4 +1,53 @@
-// from data.js
+// from data.js => an array of dictionaries (js objects)
 var tableData = data;
 
-// YOUR CODE HERE!
+var filterBtn = d3.select("#filter-btn");
+
+var dateBtn = d3.select("#date-btn");
+
+var inputDate = d3.select("#datetime");
+
+console.log(tableData.length);
+
+filterBtn.on("click", function() {
+
+  d3.select("#results-body").selectAll("tr").remove();
+
+  var inputValue = inputDate.property("value");
+
+  function matchDates(searchDate) {
+    return searchDate.datetime == inputValue;
+  }
+
+  var matching = tableData.filter(matchDates);
+
+  var tbody = d3.select("#results-body");
+
+  matching.forEach((filteredResult) => {
+    var row = tbody.append("tr");
+    Object.entries(filteredResult).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+});
+
+dateBtn.on("click", function() {
+
+  function retrieveData(entry) {
+    return entry;
+  }
+
+  var retrievedData = tableData;
+
+  var dataBody = d3.select("#all-data-body");
+  
+  retrievedData.forEach((entry) => {
+    var row = dataBody.append("tr");
+    Object.entries(entry).forEach(([key, value]) => { 
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+});
